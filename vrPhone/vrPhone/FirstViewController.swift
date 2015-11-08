99d3876ebe8bae25e362cc4ca6def42bfde8c818
김용수 <delixion@navercorp.com>
2015-11-09 01:13:28 +0900

    default new project

---
+++ vrPhone/vrPhone/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  vrPhone
+//
+//  Created by Yongsoo Kim on 2015. 11. 8..
+//  Copyright © 2015년 delixion. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
